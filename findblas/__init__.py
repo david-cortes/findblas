ea7a16986137b10fc042a5b412d87480238d0cb7
David Cortes <david.cortes.rivera@gmail.com>
2020-11-29 21:46:13 +0200

    fix bug with adding none to list

--- findblas/__init__.py
+++ findblas/__init__.py
@@ -434,14 +434,14 @@ def find_blas():
 		if blas_file is not None:
 			if platform[:3] != "win":
 				found_syms = _find_symbols(blas_path, blas_file)
-				if found_syms is not None:
+				if found_syms[1] is not None:
 					flags_found += found_syms[1]
 		else:
 			blas_path, blas_file = search_blas_lib(search_paths, [pref + "blas" + e for e in ext])
 			if blas_file is not None:
 				if platform[:3] != "win":
 					found_syms = _find_symbols(blas_path, blas_file)
-					if found_syms is not None:
+					if found_syms[1] is not None:
 						flags_found += found_syms[1]
 
 	### Try regex matching
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
 setup(
   name = 'findblas',
   packages = ['findblas'],
-  version = '0.1.16',
+  version = '0.1.17',
   author = 'David Cortes',
   url = 'https://github.com/david-cortes/findblas',
   classifiers = [],
