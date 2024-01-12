bb7edf4566341be0f2a034d520e742d545edd80c
J. Derek Tucker <jdtuck@sandia.gov>
2024-01-12 09:51:37 -0700

    fix formatting

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 setup(
-  name = 'findblas',
-  packages = ['findblas'],
-  version = '0.1.22',
-  author = 'David Cortes',
-  url = 'https://github.com/david-cortes/findblas',
-  classifiers = [],
-  data_files=[('include', ['findblas/findblas.h', 'findblas/rtd_mock.c'])],
-  include_package_data=True
-) 
+    name="findblas",
+    packages=["findblas"],
+    version="0.1.22",
+    author="David Cortes",
+    url="https://github.com/david-cortes/findblas",
+    classifiers=[],
+    data_files=[("include", ["findblas/findblas.h", "findblas/rtd_mock.c"])],
+    include_package_data=True,
+)
